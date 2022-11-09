1ca9c97a65d31ac927e423ae557eed9327fd7a1b
YoonaBae <yooona0769@gmail.com>
2022-11-09 23:00:14 +0900

    풀이 수정해야함...

---
+++ algorithmWorkSpace/algorithmStep01/src/algorithmStep01/StyProgrammers05.java
@@ -0,0 +1,38 @@
+package algorithmStep01;
+
+import java.util.Scanner;
+
+public class StyProgrammers05 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		long n = sc.nextLong();
+		
+		
+		int[] answer = solution(n);
+		
+		for(int i=0; i < answer.length; i++) {
+			System.out.println(answer[i]);
+		}
+	}
+
+	private static int[] solution(long n) {
+		
+		int length = 0;
+		while(n > 0) {
+			n /= 10;
+			
+			length += 1;
+		}
+		
+		int[] answer = new int[length];
+		
+		for(int i = 0; n != 0; i++) {
+			answer[i] = (int)(n % 10);
+			n /= 10;
+		}
+		
+		return answer;
+	}
+
+}

a28f341ab5f87a0978e85362b3a1d063a1816446
pearyoon <106965001+pearyoon@users.noreply.github.com>
2023-02-26 17:55:18 +0900

    [level 1] Title: 하샤드 수, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean solution(int x) {
+        boolean answer = true;
+        
+        int sum = 0;
+        String s = Integer.toString(x);
+        for(int i=0; i<s.length(); i++){
+            sum += Integer.parseInt(s.substring(i, i+1));
+        }
+        
+        if(x%sum!=0){
+            answer = false; 
+        }
+        
+        
+        return answer;
+    }
+}
